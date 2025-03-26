46166b5ee905d56f6af1453edc9bcc8c025334b7
Kasfex <kasfex@gmail.com>
2025-03-26 23:43:10 +0000

    site pages config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ebenai",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
